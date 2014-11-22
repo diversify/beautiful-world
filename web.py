947f28f004bd124246d03c10dc303449b1acb634
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-11-22 21:24:44 +0100

    Example of serving a Photo

--- web.py
+++ web.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+import mongoengine
+from flask import Flask, render_template, send_file
 
 import config
+from models import Photo
 
 # Initializing the web app
 app = Flask(__name__)
+# Initializing the DB
+db = mongoengine.connect(config.db_name)
 
 # Views
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/photo')
+def serve_photo():
+    photo = Photo.objects[0]
+    return send_file(photo.file, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(host=config.flask_host, port=config.flask_port, debug=config.flask_debug)
