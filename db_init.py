5d0b97b0f343b29253e874726ecd0c452e4aab1e
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-11-22 22:36:51 +0100

    Tried to limit the number of API calls in the db init script

--- db_init.py
+++ db_init.py
@@ -9,6 +9,9 @@ db = mongoengine.connect(config.db_name)
 
 PICTURES_FOLDER = 'example_pictures'
 
+def clean(string):
+    return string.lower().strip()
+
 if __name__ == '__main__':
 
     for track_photo_name in os.listdir(PICTURES_FOLDER):
@@ -21,11 +24,18 @@ if __name__ == '__main__':
 
         # Track object
         artist, title = track_photo_name.split('.')[0].split('_')
-        track = Track.get_from_spotify(artist, title)
+        # Trying to find a previously created track object
+        track = None
+        for t in Track.objects:
+            if clean(t.name) == clean(title) and clean(t.artist_name) == clean(title):
+                track = t
+                break
         if track is None:
-            print "! Couldn't get info for '{} - {}' ; skipping.".format(artist, title)
-            continue
-        track.save()
+            track = Track.get_from_spotify(artist, title)
+            if track is None:
+                print "! Couldn't get info for '{} - {}' ; skipping.".format(artist, title)
+                continue
+            track.save()
 
         # Submission
         submission = Submission(photo=photo, track=track)
