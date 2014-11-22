dd055cb05960f9ad3fd3969a8a989855a946673d
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-11-23 00:58:09 +0100

    View and backend for the list of submissions (with images, info, etc.)

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ import mongoengine
 from flask import Flask, render_template, send_file
 
 import config
-from models import Photo, Track
+from models import Submission
 
 # Initializing the web app
 app = Flask(__name__)
@@ -18,17 +18,21 @@ mongoengine.Document.to_dict = lambda d : json.loads(d.to_json())
 def index():
     return render_template('index.html')
 
+@app.route('/submissions')
+def submissions_view():
+    return render_template('submissions.html', submissions=Submission.objects)
+
 @app.route('/preview/<template>')
 def preview(template):
     return render_template(template)
 
-@app.route('/api/tracks')
-def tracks():
-    return json.dumps([track.to_dict() for track in Track.objects])
+@app.route('/api/submissions')
+def api_submissions():
+    return json.dumps([dict(track=sub.track.to_dict(), id=str(sub.id)) for sub in Submission.objects])
 
-@app.route('/photo')
-def serve_photo():
-    photo = Photo.objects[0]
+@app.route('/api/submission/<submission_id>/photo')
+def api_submission_photo(submission_id):
+    photo = Submission.objects.get(id=submission_id).photo
     return send_file(photo.file, mimetype='image/jpeg')
 
 if __name__ == '__main__':
